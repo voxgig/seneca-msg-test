528231e76ca181cf1fe26dde12b88afca4c4bd03
Richard Rodger <richard@ricebridge.com>
2018-10-06 11:05:19 +0100

    v0.2.0

--- msg-test.js
+++ msg-test.js
@@ -38,8 +38,11 @@ function msg_test(seneca, spec) {
 
   // top level `pattern` replaces `fix`; `fix` deprecated as does not override
   spec.pattern = '' === spec.pattern ? spec.fix : spec.pattern
-  
-  return function(fin) {
+
+  test.run = intern.run
+  return test
+
+  function test(fin) {
     if(spec.test) {
       seneca
         .test(fin, spec.log ? 'print' : null)
@@ -73,111 +76,118 @@ function msg_test(seneca, spec) {
             }
           }
 
-
-          var callmap = spec.context
-          next_call(0)
-          
-          function next_call(call_index) {
-            if(spec.calls.length <= call_index) {
-              return fin()
-            }
-
-            var call = spec.calls[call_index]
-
-            if(false === call.run) {
-              return setImmediate(next_call.bind(null,call_index+1))
-            }
-
-            var params = {}
-            Object.keys(call.params).forEach(function(pk) {
-              var pv = call.params[pk]
-
-              pk = Inks(pk,callmap)
-              if('string' === typeof pv) {
-                pv = Inks(pv,callmap)
-              }
-              
-              params[pk] = pv
-            })
-
-            var print = spec.print || call.print
-            
-            if(print) {
-              console.log('\n\nCALL   : ', call.pattern, params)
-            }
-
-            if(call.print_context) {
-              console.dir(callmap, {depth:3,colors:true})
-            }
-            
-            var msg = Object.assign(
-              {},
-              params,
-              Jsonic(spec.pattern),
-              Jsonic(call.pattern)
-            )
-            var msgstr = Jsonic.stringify(msg)
-            
-            seneca.act(msg, function(err, out, meta) {
-              if(print) {
-                console.log('ERROR  : ', err)
-                console.log('RESULT : ', Util.inspect(out,{depth:null,colors:true}))
-              }
-
-              if(null == call.err) {
-                if(null != err ) {
-                  return fin(new Error('Error not expected for: '+
-                                       msgstr+', err: '+err))
-                }
-              }
-              else {
-                if(null == err ) {
-                  return fin(new Error('Error expected for: '+
-                                       msgstr+', was null'))
-                }
-
-          
-                var result = Optioner(call.err,{must_match_literals:true})(err)
-                if(result.error) {
-                  return fin(result.error)
-                }
-              }
-              
-              if(null == call.out) {
-                if(null != out ) {
-                  return fin(new Error('Output not expected for: '+
-                                       msgstr+', out: '+out))
-                }
-              }
-              else {
-                if(null == out ) {
-                  return fin(new Error('Output expected for: '+
-                                       msgstr+', was null'))
-                }
-
-
-                var result = Optioner(call.out,{must_match_literals:true})(out)
-                if(result.error) {
-                  return fin(new Error('Output for: '+msgstr+
-                                       ' was invalid: '+result.error.message))
-                }
-              }
-
-              if(call.name) {
-                callmap[call.name] = {
-                  top_pattern: spec.pattern,
-                  pattern: call.pattern,
-                  params: params,
-                  msg: msg,
-                  err: err,
-                  out: out,
-                  meta: meta
-                }
-              }
-              
-              setImmediate(next_call.bind(null,call_index+1))
-            })
-          }
+          intern.run(seneca, spec, fin)
         })
   }
 }
+
+
+const intern = (module.exports.intern = {
+
+  run: async function(seneca, spec, done) {
+    var callmap = spec.context
+    next_call(0)
+    
+    function next_call(call_index) {
+      if(spec.calls.length <= call_index) {
+        return done()
+      }
+
+      var call = spec.calls[call_index]
+
+      if(false === call.run) {
+        return setImmediate(next_call.bind(null,call_index+1))
+      }
+
+      var params = {}
+      Object.keys(call.params).forEach(function(pk) {
+        var pv = call.params[pk]
+
+        pk = Inks(pk,callmap)
+        if('string' === typeof pv) {
+          pv = Inks(pv,callmap)
+        }
+        
+        params[pk] = pv
+      })
+
+      var print = spec.print || call.print
+      
+      if(print) {
+        console.log('\n\nCALL   : ', call.pattern, params)
+      }
+
+      if(call.print_context) {
+        console.dir(callmap, {depth:3,colors:true})
+      }
+      
+      var msg = Object.assign(
+        {},
+        params,
+        Jsonic(spec.pattern),
+        Jsonic(call.pattern)
+      )
+      var msgstr = Jsonic.stringify(msg)
+      
+      seneca.act(msg, function(err, out, meta) {
+        if(print) {
+          console.log('ERROR  : ', err)
+          console.log('RESULT : ', Util.inspect(out,{depth:null,colors:true}))
+        }
+
+        if(null == call.err) {
+          if(null != err ) {
+            return done(new Error('Error not expected for: '+
+                                 msgstr+', err: '+err))
+          }
+        }
+        else {
+          if(null == err ) {
+            return done(new Error('Error expected for: '+
+                                 msgstr+', was null'))
+          }
+
+          
+          var result = Optioner(call.err,{must_match_literals:true})(err)
+          if(result.error) {
+            return done(result.error)
+          }
+        }
+        
+        if(null == call.out) {
+          if(null != out ) {
+            return done(new Error('Output not expected for: '+
+                                 msgstr+', out: '+out))
+          }
+        }
+        else {
+          if(null == out ) {
+            return done(new Error('Output expected for: '+
+                                 msgstr+', was null'))
+          }
+
+
+          var result = Optioner(call.out,{must_match_literals:true})(out)
+          if(result.error) {
+            return done(new Error('Output for: '+msgstr+
+                                 ' was invalid: '+result.error.message))
+          }
+        }
+
+        if(call.name) {
+          callmap[call.name] = {
+            top_pattern: spec.pattern,
+            pattern: call.pattern,
+            params: params,
+            msg: msg,
+            err: err,
+            out: out,
+            meta: meta
+          }
+        }
+        
+        setImmediate(next_call.bind(null,call_index+1))
+      })
+    }
+  }
+})
