37fb77f1068ed88aa42042be5132141bfac6009b
rjrodger <richard@ricebridge.com>
2019-05-09 02:32:38 +0100

    delegates

--- msg-test.js
+++ msg-test.js
@@ -1,6 +1,5 @@
-/* Copyright (c) 2018 voxgig and other contributors, MIT License */
+/* Copyright (c) 2018-2019 voxgig and other contributors, MIT License */
 'use strict'
-
 
 const Util = require('util')
 
@@ -9,28 +8,37 @@ const Inks = require('inks')
 const Optioner = require('optioner')
 const Joi = Optioner.Joi
 
-
-
 module.exports = msg_test
 module.exports.Joi = Joi
-
 
 const optioner = Optioner({
   test: Joi.boolean().default(true),
   log: Joi.boolean().default(false),
-  data: Joi.object().unknown().default({}),
-  context: Joi.object().unknown().default({}),
+  data: Joi.object()
+    .unknown()
+    .default({}),
+  context: Joi.object()
+    .unknown()
+    .default({}),
   fix: Joi.string().default(''), // DEPRECATED, use pattern instead
   pattern: Joi.string().default(''),
-  calls: Joi.array().items(Joi.object({
-    name: Joi.string().min(1),
-    print: Joi.boolean().default(false),
-    print_context: Joi.boolean().default(false),
-    pattern: Joi.string().min(3),
-    params: Joi.object().unknown().default({}),
-    out: Joi.alternatives().try(Joi.object().unknown(),Joi.array()),
-    err: Joi.object().unknown(),
-  }))
+  delegates: Joi.object()
+    .pattern(/^/, Joi.array().items(Joi.object().allow(null)))
+    .default({}),
+  calls: Joi.array().items(
+    Joi.object({
+      name: Joi.string().min(1),
+      print: Joi.boolean().default(false),
+      print_context: Joi.boolean().default(false),
+      pattern: Joi.string().min(3),
+      params: Joi.object()
+        .unknown()
+        .default({}),
+      out: Joi.alternatives().try(Joi.object().unknown(), Joi.array()),
+      err: Joi.object().unknown(),
+      delegate: Joi.string()
+    })
+  )
 })
 
 function msg_test(seneca, spec) {
@@ -42,85 +50,99 @@ function msg_test(seneca, spec) {
   test.run = intern.run
   return test
 
-  function test(fin) {
-    if(spec.test) {
-      seneca
-        .test(fin, spec.log ? 'print' : null)
+  async function test() {
+    if (spec.test) {
+      seneca.test(null, spec.log ? 'print' : null)
+    }
+
+    if (!seneca.has_plugin('promisify')) {
+      seneca.use('promisify')
+      await seneca.ready()
     }
 
     // TODO: make optional
     var datajson = JSON.stringify(spec.data)
 
-    seneca
-      .act(
-        'role:mem-store,cmd:import',
-        { json: datajson, default$: {} },
-        function() {
-          seneca = this
+    await seneca.post('role:mem-store,cmd:import', {
+      json: datajson,
+      default$: {}
+    })
 
-          const foundmsgs = seneca
-                .list(spec.pattern)
-                .map(msg=>seneca.util.pattern(msg))
+    const foundmsgs = seneca
+      .list(spec.pattern)
+      .map(msg => seneca.util.pattern(msg))
 
-          const specmsgs = {}
+    const specmsgs = {}
 
-          spec.calls.forEach(call=>{
-            var specmsg = seneca.util.pattern(Jsonic(spec.pattern+','+call.pattern))
-            specmsgs[specmsg] = true
-          })
+    spec.calls.forEach(call => {
+      var specmsg = seneca.util.pattern(
+        Jsonic(spec.pattern + ',' + call.pattern)
+      )
+      specmsgs[specmsg] = true
+    })
 
-          for(var i = 0; i < foundmsgs.length; i++) {
-            var msg = foundmsgs[i]
-            if(null == specmsgs[msg]) {
-              return fin(new Error('Test calls not defined for: '+msg))
-            }
-          }
+    for (var i = 0; i < foundmsgs.length; i++) {
+      var msg = foundmsgs[i]
+      if (null == specmsgs[msg]) {
+        throw new Error('Test calls not defined for: ' + msg)
+      }
+    }
 
-          intern.run(seneca, spec, fin)
-        })
+    Object.keys(spec.delegates).forEach(dk => {
+      spec.delegates[dk] = seneca.delegate.apply(seneca, spec.delegates[dk])
+    })
+
+    await intern.run(seneca, spec)
   }
 }
 
-
 const intern = (module.exports.intern = {
-
-  run: function(seneca, spec, done) {
+  run: async function(seneca, spec) {
     var callmap = spec.context
-    next_call(0)
-    
-    function next_call(call_index) {
-      if(spec.calls.length <= call_index) {
+
+    return new Promise((resolve, reject) => {
+      next_call(0, function(err) {
+        if (err) {
+          return reject(err)
+        } else {
+          return resolve()
+        }
+      })
+    })
+
+    function next_call(call_index, done) {
+      if (spec.calls.length <= call_index) {
         return done()
       }
 
       var call = spec.calls[call_index]
 
-      if(false === call.run) {
-        return setImmediate(next_call.bind(null,call_index+1))
+      if (false === call.run) {
+        return setImmediate(next_call.bind(null, call_index + 1, done))
       }
 
       var params = {}
       Object.keys(call.params).forEach(function(pk) {
         var pv = call.params[pk]
 
-        pk = Inks(pk,callmap)
-        if('string' === typeof pv) {
-          pv = Inks(pv,callmap)
+        pk = Inks(pk, callmap)
+        if ('string' === typeof pv) {
+          pv = Inks(pv, callmap)
         }
-        
+
         params[pk] = pv
       })
 
       var print = spec.print || call.print
-      
-      if(print) {
+
+      if (print) {
         console.log('\n\nCALL   : ', call.pattern, params)
       }
 
-      if(call.print_context) {
-        console.dir(callmap, {depth:3,colors:true})
+      if (call.print_context) {
+        console.dir(callmap, { depth: 3, colors: true })
       }
-      
+
       var msg = Object.assign(
         {},
         params,
@@ -128,53 +150,72 @@ const intern = (module.exports.intern = {
         Jsonic(call.pattern)
       )
       var msgstr = Jsonic.stringify(msg)
-      
-      seneca.act(msg, function(err, out, meta) {
-        if(print) {
+
+      var instance = seneca
+
+      if (call.delegate) {
+        instance = spec.delegates[call.delegate]
+
+        if (null == instance) {
+          return done(new Error('Delegate not defined: ' + call.delegate))
+        }
+      }
+
+      instance.act(msg, function(err, out, meta) {
+        if (print) {
           console.log('ERROR  : ', err)
-          console.log('RESULT : ', Util.inspect(out,{depth:null,colors:true}))
+          console.log(
+            'RESULT : ',
+            Util.inspect(out, { depth: null, colors: true })
+          )
         }
 
-        if(null == call.err) {
-          if(null != err ) {
-            return done(new Error('Error not expected for: '+
-                                 msgstr+', err: '+err))
+        if (null == call.err) {
+          if (null != err) {
+            return done(
+              new Error('Error not expected for: ' + msgstr + ', err: ' + err)
+            )
           }
-        }
-        else {
-          if(null == err ) {
-            return done(new Error('Error expected for: '+
-                                 msgstr+', was null'))
+        } else {
+          if (null == err) {
+            return done(
+              new Error('Error expected for: ' + msgstr + ', was null')
+            )
           }
 
-          
-          var result = Optioner(call.err,{must_match_literals:true})(err)
-          if(result.error) {
+          var result = Optioner(call.err, { must_match_literals: true })(err)
+          if (result.error) {
             return done(result.error)
           }
         }
-        
-        if(null === call.out) {
-          if(null != out ) {
-            return done(new Error('Output not expected for: '+
-                                 msgstr+', out: '+out))
+
+        if (null === call.out) {
+          if (null != out) {
+            return done(
+              new Error('Output not expected for: ' + msgstr + ', out: ' + out)
+            )
           }
-        }
-        else if(null != call.out) {
-          if(null == out ) {
-            return done(new Error('Output expected for: '+
-                                 msgstr+', was null'))
-          }
-          else {
-            var result = Optioner(call.out,{must_match_literals:true})(out)
-            if(result.error) {
-              return done(new Error('Output for: '+msgstr+
-                                    ' was invalid: '+result.error.message))
+        } else if (null != call.out) {
+          if (null == out) {
+            return done(
+              new Error('Output expected for: ' + msgstr + ', was null')
+            )
+          } else {
+            var result = Optioner(call.out, { must_match_literals: true })(out)
+            if (result.error) {
+              return done(
+                new Error(
+                  'Output for: ' +
+                    msgstr +
+                    ' was invalid: ' +
+                    result.error.message
+                )
+              )
             }
           }
         }
 
-        if(call.name) {
+        if (call.name) {
           callmap[call.name] = {
             top_pattern: spec.pattern,
             pattern: call.pattern,
@@ -185,8 +226,8 @@ const intern = (module.exports.intern = {
             meta: meta
           }
         }
-        
-        setImmediate(next_call.bind(null,call_index+1))
+
+        setImmediate(next_call.bind(null, call_index + 1, done))
       })
     }
   }
--- test/msg-test.test.js
+++ test/msg-test.test.js
@@ -1,4 +1,4 @@
-/* Copyright © 2018 voxgig ltd. CONFIDENTIAL AND PROPRIETARY. */
+/* Copyright © 2018-2019 voxgig ltd. CONFIDENTIAL AND PROPRIETARY. */
 'use strict'
 
 const Util = require('util')
@@ -12,100 +12,152 @@ const SenecaMsgTest = require('../')
 const Seneca = require('seneca')
 const Joi = SenecaMsgTest.Joi
 
-
-lab.test('happy', SenecaMsgTest(
-  seneca_instance({log:'silent'},function(seneca) {
-    return seneca
-      .use(function plugin0() {
-        this
-          .add('role:plugin0,cmd:zed', function(msg, reply) {
-            this.make$('foo/bar').load$(msg.bid, reply)
-          })
-          .add('role:plugin0,cmd:qaz',function(msg, reply){
-            reply({x:1,y:msg.y})
-          })
+lab.test(
+  'happy',
+  SenecaMsgTest(
+    seneca_instance({ log: 'silent' }, function(seneca) {
+      return seneca.use(function plugin0() {
+        this.add('role:plugin0,cmd:zed', function(msg, reply) {
+          this.make$('foo/bar').load$(msg.bid, reply)
+        }).add('role:plugin0,cmd:qaz', function(msg, reply) {
+          reply({ x: 1, y: msg.y })
+        })
       })
-  }),
-  {
-    test: true,
-    data: { foo: { bar: {
-      b0: {entity$:'-/foo/bar', id:'b0', b:0},
-      b1: {entity$:'-/foo/bar', id:'b1', b:1} }}},
-    pattern: 'role:plugin0',
-    calls: [
-      {
-        pattern: 'cmd:zed',
-        params: { bid: 'b0' },
-        out: { b: 0 }
+    }),
+    {
+      test: true,
+      data: {
+        foo: {
+          bar: {
+            b0: { entity$: '-/foo/bar', id: 'b0', b: 0 },
+            b1: { entity$: '-/foo/bar', id: 'b1', b: 1 }
+          }
+        }
       },
-      {
-        pattern: 'cmd:qaz',
-        params: { y:'a' },
-        out: { x:1, y:'a' }
+      pattern: 'role:plugin0',
+      calls: [
+        {
+          pattern: 'cmd:zed',
+          params: { bid: 'b0' },
+          out: { b: 0 }
+        },
+        {
+          pattern: 'cmd:qaz',
+          params: { y: 'a' },
+          out: { x: 1, y: 'a' }
+        }
+      ]
+    }
+  )
+)
+
+lab.test(
+  'delegates',
+  SenecaMsgTest(
+    seneca_instance({ log: 'silent' }, function(seneca) {
+      return seneca.use(function plugin0() {
+        this.add('role:plugin0,cmd:qaz', function(msg, reply, meta) {
+          reply({ x: 1, y: msg.y, z: meta.custom.z, w: msg.w })
+        })
+      })
+    }),
+    {
+      test: true,
+      print: false,
+      delegates: {
+        d0: [{ w: 'AA' }, { custom: { z: 'A' } }],
+        d1: [{ w: 'BB' }, { custom: { z: 'B' } }]
       },
-    ]
-  }))
+      pattern: 'role:plugin0',
+      calls: [
+        {
+          delegate: 'd0',
+          pattern: 'cmd:qaz',
+          params: { y: 'a' },
+          out: { x: 1, y: 'a', z: 'A', w: 'AA' }
+        },
+        {
+          delegate: 'd1',
+          pattern: 'cmd:qaz',
+          params: { y: 'b' },
+          out: { x: 1, y: 'b', z: 'B', w: 'BB' }
+        },
+        {
+          delegate: 'd0',
+          pattern: 'cmd:qaz',
+          params: { y: 'c' },
+          out: { x: 1, y: 'c', z: 'A', w: 'AA' }
+        },
+        {
+          delegate: 'd1',
+          pattern: 'cmd:qaz',
+          params: { y: 'd' },
+          out: { x: 1, y: 'd', z: 'B', w: 'BB' }
+        }
+      ]
+    }
+  )
+)
 
-
-lab.test('data-sequence', SenecaMsgTest(
-  seneca_instance({log:'silent'},function(seneca) {
-    return seneca
-      .use(function foo() {
-        this
-          .add('role:foo,cmd:add', function(msg, reply) {
-            this.make$('foo').data$(msg.data).save$(reply)
-          })
+lab.test(
+  'data-sequence',
+  SenecaMsgTest(
+    seneca_instance({ log: 'silent' }, function(seneca) {
+      return seneca.use(function foo() {
+        this.add('role:foo,cmd:add', function(msg, reply) {
+          this.make$('foo')
+            .data$(msg.data)
+            .save$(reply)
+        })
           .add('role:foo,cmd:get', function(msg, reply) {
             this.make$('foo').load$(msg.id, reply)
           })
-          .add('role:foo,cmd:list',function(msg, reply) {
+          .add('role:foo,cmd:list', function(msg, reply) {
             this.make$('foo').list$(msg.q, reply)
           })
-          .add('role:foo,cmd:err',function(msg, reply) {
+          .add('role:foo,cmd:err', function(msg, reply) {
             reply(new Error(msg.text))
           })
       })
-  }),
-  {
-    print: false,
-    test: false,
-    data: {},
-    pattern: 'role:foo',
-    calls: [
-      {
-        name:'foo/a1',
-        pattern: 'cmd:add',
-        params: { data: {a:1,b:'A'} },
-        out: { entity$:'-/-/foo', a:1, b:'A' }
-      },
-      {
-        pattern: 'cmd:get',
-        params: { id:'`foo/a1:out.id`' },
-        out: { entity$:'-/-/foo', a:1, b:'A' }
-      },
-      {
-        pattern: 'cmd:list',
-        params: {q:{a:1}},
-        out: [{a:1}]
-      },
-      // don't care about output
-      {
-        pattern: 'cmd:list',
-        params: {q:{a:1}},
-      },
-      {
-        pattern: 'cmd:err',
-        params: {text:'foo'},
-        err: {message:'seneca: Action cmd:err,role:foo failed: foo.'}
-      }
-    ]
-  }))
-
-
+    }),
+    {
+      print: false,
+      test: false,
+      data: {},
+      pattern: 'role:foo',
+      calls: [
+        {
+          name: 'foo/a1',
+          pattern: 'cmd:add',
+          params: { data: { a: 1, b: 'A' } },
+          out: { entity$: '-/-/foo', a: 1, b: 'A' }
+        },
+        {
+          pattern: 'cmd:get',
+          params: { id: '`foo/a1:out.id`' },
+          out: { entity$: '-/-/foo', a: 1, b: 'A' }
+        },
+        {
+          pattern: 'cmd:list',
+          params: { q: { a: 1 } },
+          out: [{ a: 1 }]
+        },
+        // don't care about output
+        {
+          pattern: 'cmd:list',
+          params: { q: { a: 1 } }
+        },
+        {
+          pattern: 'cmd:err',
+          params: { text: 'foo' },
+          err: { message: 'seneca: Action cmd:err,role:foo failed: foo.' }
+        }
+      ]
+    }
+  )
+)
 
 // TODO: provide in common-server-test
 function seneca_instance(options, setup) {
-  return setup( Seneca(options)
-                .use('entity') )
+  return setup(Seneca(options).use('entity'))
 }
-
